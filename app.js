c5f4bbbd7a1926d4f026c4a121001337b8823631
Kaspar Jarosz <40368976@eeecs.qub.ac.uk>
2023-10-24 15:00:14 +0100

    add new files

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+
+
+app.set('view engine', 'ejs');
+
+app.get("/", async(req, res) => {
+    let url = `http://universities.hipolabs.com/search?country=australia`;
+
+    let response = await fetch(url); // use the fetch method
+
+    let unis = await response.json(); // read response body and parse as JSON
+
+    //console.table(unis[0].name);
+
+    //let firstuni = unis[0];
+
+
+    res.render("index", {uni_data: unis});
+});
+
+app.listen(3000, () => {
+    console.log("Server is listening on port localhost:3000");
+});
+
+
